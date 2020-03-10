7649bd292ea2efa5eb14985edeec5b69971acdf8
kumar saraboji <kumar.saraboji@gmail.com>
2020-03-10 17:48:51 +0000

    first commit

---
+++ consumers/models/.ipynb_checkpoints/weather-checkpoint.py
@@ -0,0 +1,25 @@
+"""Contains functionality related to Weather"""
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class Weather:
+    """Defines the Weather model"""
+
+    def __init__(self):
+        """Creates the weather model"""
+        self.temperature = 70.0
+        self.status = "sunny"
+
+    def process_message(self, message):
+        """Handles incoming weather data"""
+        
+        try:
+            value = message.value()
+            self.temperature = value.temperature
+            self.status = value.status
+        except Exception as e:
+            logger.error(f"weather process_message is incomplete - skipping. reason = {str(e)}")
+  
---
+++ producers/models/turnstile.py
@@ -0,0 +1,57 @@
+"""Creates a turnstile data producer"""
+import logging
+from pathlib import Path
+
+from confluent_kafka import avro
+
+from models.producer import Producer
+from models.turnstile_hardware import TurnstileHardware
+
+
+logger = logging.getLogger(__name__)
+
+
+class Turnstile(Producer):
+    key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
+
+    value_schema = avro.load(
+       f"{Path(__file__).parents[0]}/schemas/turnstile_value.json")
+
+    def __init__(self, station):
+        """Create the Turnstile"""
+        station_name = (
+            station.name.lower()
+            .replace("/", "_and_")
+            .replace(" ", "_")
+            .replace("-", "_")
+            .replace("'", "")
+        )
+
+        super().__init__(
+            f"cta_turnstiles", 
+            key_schema=Turnstile.key_schema,
+            value_schema=Turnstile.value_schema, 
+            num_partitions=3,
+            num_replicas=1,
+        )
+        self.station = station
+        self.turnstile_hardware = TurnstileHardware(station)
+
+    def run(self, timestamp, time_step):
+        """Simulates riders entering through the turnstile."""
+        
+        num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
+        try:
+            for _ in range(num_entries):
+                self.producer.produce(
+                    topic=f"cta_turnstiles",
+                    key={"timestamp" : self.time_millis()},
+                    value={
+                        "station_id" : self.station.station_id,
+                        "station_name" : self.station.station_name,
+                        "line" : self.station.color.name,
+                    },
+                )
+            logger.info(f"{self.station.station_id}-{self.station.station_name}-{num_entries}")
+        except Exception as e:
+            logger.error("turnstile kafka integration incomplete - skipping")
